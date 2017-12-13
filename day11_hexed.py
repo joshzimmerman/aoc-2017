17c77dd8f679f93d63e818878b13e6ff60384f74
Josh Zimmerman <joshz@joshz.org>
2017-12-12 18:48:16 -0800

    faster day11

--- day11_hexed.py
+++ day11_hexed.py
@@ -1,20 +1,6 @@
+import collections
 import sys
 
-
-# order irrelevant
-# anything twice can't be reduced
-# n sw = nw
-# n se = ne
-# nw s = sw
-# nw ne = n
-# sw se = s
-# sw n = nw
-# s nw = sw
-# s ne = se
-# se n = ne
-
-OPPOSITES = {"n": "s", "ne": "sw", "nw": "se",
-             "s": "n", "sw": "ne", "se": "nw"}
 SIMPLIFIERS = {"n": {"se": "ne", "sw": "nw"},
                "ne": {"s": "se", "nw": "n"},
                "se" : {"n": "ne", "sw": "s"},
@@ -22,52 +8,35 @@ SIMPLIFIERS = {"n": {"se": "ne", "sw": "nw"},
                "sw": {"n": "nw", "se": "s"},
                "nw": {"ne": "n", "s": "sw"}}
 
-def excise_opposites(path):
-    i = 0
-    while i < len(path):
-        opp = OPPOSITES[path[i]]
-        try:
-            opp_idx = path.index(opp, i)
-            path = path[:i] + path[i + 1:opp_idx] + path[opp_idx + 1:]
-        except ValueError:
-            i += 1
-    return path
-
-def simplify_sequences(path):
-    i = 0
-    while i < len(path):
-        simps = SIMPLIFIERS[path[i]]
-        for s in simps:
-            try:
-                idx = path.index(s, i)
-                replace = simps[path[idx]]
-                path = path[:i] + path[i + 1:idx] + [replace] + path[idx + 1:]
-                break
-            except ValueError:
-                continue
+def simplify(d):
+    OPPS = [('n', 's'), ('ne', 'sw'), ('nw', 'se')]
+    for a, b in OPPS:
+        if d[a] > d[b]:
+            d[a] -= d[b]
+            d[b] = 0
         else:
-            i += 1
-    return path
+            d[b] -= d[a]
+            d[a] = 0
+    for step in SIMPLIFIERS:
+        if d[step] > 0:
+            for other in SIMPLIFIERS[step]:
+                simp_count = min(d[step], d[other])
+                d[step] -= simp_count
+                d[other] -= simp_count
+                d[SIMPLIFIERS[step][other]] += simp_count
+    return d
 
 def part_1(path):
-    path = excise_opposites(path)
-    path = simplify_sequences(path)
-    return path
-
-def part_2(path):
-    i = 0
-    large = 0
-    mid = path
-    while i < len(path):
-        mid = part_1(path[:i])
-        if len(mid) > large:
-            large = len(mid)
-        path = mid + path[i:]
-        i = len(mid) + 1
-    return large
+    d = collections.defaultdict(int)
+    highest = 0
+    for step in path:
+        d[step] += 1
+        simplify(d)
+        if sum(d.values()) > highest:
+            highest = sum(d.values())
+    simplify(d)
+    return sum(d.values()), highest
 
 if __name__ == "__main__":
     path = sys.argv[1].split(",")
-    print len(part_1(path))
-    print part_2(path)
-    print len(path)
+    print part_1(path)
