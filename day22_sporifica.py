12d77cf15b11000acc895dff52733500551bf854
Josh Zimmerman <joshz@joshz.org>
2017-12-23 09:40:06 -0800

    Do day22 part 2

--- day22_sporifica.py
+++ day22_sporifica.py
@@ -1,41 +1,50 @@
+import collections
 import sys
 
 DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+STATES = [".", "W", "#", "F"]
 ITERATIONS = 10000
 
 def tuple_add((a,b), (c,d)):
     return (a + c, b + d)
 
-def burst(board, cur, face):
-    if cur in board:
+def burst(board, cur, face, state_step_size):
+    state = board[cur]
+    if state == ".":
+        # Turn left
+        face = (face - 1) % len(DIRECTIONS)
+    elif state == "#":
         # Turn right
         face = (face + 1) % len(DIRECTIONS)
-        board.remove(cur)
-        infected = False
-    else:
-        face = (face - 1) % len(DIRECTIONS)
-        board.add(cur)
-        infected = True
-    return board, tuple_add(cur, DIRECTIONS[face]), face, infected
+    elif state == "F":
+        # Turn around
+        face = (face + 2) % len(DIRECTIONS)
 
+    board[cur] = STATES[(STATES.index(state) + state_step_size) % len(STATES)]
+    return board, tuple_add(cur, DIRECTIONS[face]), face, board[cur] == "#"
 
-def part_1(board, cur):
+def part_1(board, cur, state_step_size=2, iters=ITERATIONS):
     infecting_bursts = 0
     face = 0
-    for i in xrange(ITERATIONS):
-        board, cur, face, infected = burst(board, cur, face)
+    for i in xrange(iters):
+        board, cur, face, infected = burst(board, cur, face, state_step_size)
         infecting_bursts += int(infected)
     return infecting_bursts
+
+def part_2(board, cur):
+    return part_1(board, cur, state_step_size=1, iters=10000000)
 
 if __name__ == "__main__":
     row = 0
     cols = 0
-    board = set()
+    board = collections.defaultdict(lambda: ".")
     for l in sys.stdin:
-        cols = len(l.strip())
-        for j in xrange(len(l)):
-            if l[j] == "#":
-                board.add((row, j))
+        if not cols:
+            cols = len(l.strip())
+        for j in xrange(cols):
+            board[(row, j)] = l[j]
         row += 1
+
     cur = (row / 2, cols / 2)
-    print part_1(board, cur)
+    print part_1(board.copy(), cur)
+    print part_2(board, cur)
