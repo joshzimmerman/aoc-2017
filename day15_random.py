b89691b0a19705fbbedbc8c7b963319f02724796
Josh Zimmerman <joshz@joshz.org>
2017-12-23 17:23:57 -0800

    Add day 15 input and run day 15

--- day15_random.py
+++ day15_random.py
@@ -1,8 +1,7 @@
+import sys
 INT_MAX = 0x7fffffff
 LOW_16_MASK = 0xffff
 
-GEN_A_START = 703
-GEN_B_START = 516
 NUM_TO_COMPARE = 5000000
 
 def generator(start, mul, mult_of):
@@ -12,14 +11,20 @@ def generator(start, mul, mult_of):
         if val % mult_of == 0:
             yield val
 
-if __name__ == "__main__":
-    generator_a = generator(GEN_A_START, 16807, 4)
-    generator_b = generator(GEN_B_START, 48271, 8)
+def compare(num_to_compare, a_mul=1, b_mul=1):
+    generator_a = generator(GEN_A_START, 16807, a_mul)
+    generator_b = generator(GEN_B_START, 48271, b_mul)
     same = 0
-    for i in xrange(NUM_TO_COMPARE):
+    for i in xrange(num_to_compare):
         a = next(generator_a)
         b = next(generator_b)
         if (a & LOW_16_MASK) == (b & LOW_16_MASK):
             same += 1
-    print same
+    return same
+
+if __name__ == "__main__":
+    GEN_A_START = int(sys.stdin.readline().split()[-1])
+    GEN_B_START = int(sys.stdin.readline().split()[-1])
+    print compare(40000000)
+    print compare(5000000, a_mul=4, b_mul=8)
 
